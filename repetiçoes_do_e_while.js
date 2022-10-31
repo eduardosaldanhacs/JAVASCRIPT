bb086873dc980303c85789e747473326797ecd9d
Eduardo Saldanha <102400077+eduardosaldanhacs@users.noreply.github.com>
2022-10-31 16:33:56 -0300

    Exercícios - Aula 013 e 014

---
+++ repetiçoes_do_e_while.js
@@ -0,0 +1,13 @@
+/* ESSE AQUI TESTA E EXECUTA
+var c = 1
+while (c <= 50) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+ESSE AQUI EXECUTA E DEPOIS TESTA */
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 50) 
